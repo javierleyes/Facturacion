c317df3d6cf120ed0af2c336fc6f1fd71d66be74
javier <javier.nunezleyes@gmail.com>
2019-09-29 11:41:47 -0300

    base repository

---
+++ Cargos/Cargo.Infraesctructure/Base/IBaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using Cargos.Domain.Base;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Cargos.Infrastructure.Base
+{
+    public interface IBaseRepository<TEntity> : IBaseRepository<TEntity, long> where TEntity : IDomain<long>
+    { }
+
+    public interface IBaseRepository<TEntity, TKey> 
+        where TEntity : IDomain<TKey> 
+        where TKey : IEquatable<TKey>
+    {
+        TEntity GetById(TKey id);
+
+        IQueryable<TEntity> GetAll();
+
+        IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> predicate);
+
+        TEntity Save(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+    }
+}
