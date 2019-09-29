262a409c08c80e6ee233db34b5216b9c17689072
javier <javier.nunezleyes@gmail.com>
2019-09-29 14:24:09 -0300

    Data contacts usuarios

--- Usuarios/Usuarios/DataContract/UsuarioOutputDataContract.cs
+++ Usuarios/Usuarios/DataContract/UsuarioOutputDataContract.cs
@@ -1,12 +1,11 @@
-﻿using Usuarios.Domain.Base;
-
-namespace Usuarios.Domain
+﻿namespace Usuarios.API.DataContract
 {
-    public class Usuario : Domain<long>
+    public class UsuarioOutputDataContract
     {
         public string Name { get; set; }
         public string Last_Name { get; set; }
         public int Age { get; set; }
         public string Email { get; set; }
+        public string State { get; set; }
     }
 }
